59de64f718d601f0f7238d20b0ac819147f5c384
quinncmk <quinncmk@udel.edu>
2015-11-03 18:04:51 -0500

    New

---
+++ BorgataBLL/src/main/java/pokerBase/Player.java
@@ -0,0 +1,12 @@
+package pokerBase;
+
+import domain.PlayerDomainModel;
+
+public class Player extends PlayerDomainModel {
+
+	public Player(String PlayerName, int iPlayerPosition) {
+		super(PlayerName, iPlayerPosition);
+	}
+	
+
+}
